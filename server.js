99a373a6a364b8d57714203c3a5abd8fc765847a
ian-yitzhak <onyangoian71@gmail.com>
2021-06-24 19:29:16 +0300

    Database setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const indexRouter = require('./routes/index')
+require('./db/db')
 
 app.set("view engine" , "ejs")
 app.set("views", __dirname + '/views')
