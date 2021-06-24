7564d11fec95a8b18c9949d7f511dc011c8fc4a0
ian-yitzhak <onyangoian71@gmail.com>
2021-06-24 19:08:55 +0300

    Set up the index router'

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
+const indexRouter = require('./routes/index')
 
 app.set("view engine" , "ejs")
+app.set("views", __dirname + '/views')
+app.set("layout", "layouts/layout")
+app.use(expressLayouts)
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
+
+
+app.use('/', indexRouter)
 
 const port = 3000;
 app.listen(port, ()=> console.log(`listening on ${port}`))
