4d1c45ef3f30f4e7ee32f06bd3e1dc2ead888873
ian-yitzhak <onyangoian71@gmail.com>
2021-06-25 17:51:40 +0300

    Add search functionality

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const rental = require('../models/rental')
 
-router.get('/', (req,res)=>{
-	res.render('index')
+router.get('/',async (req,res)=>{
+	let search = {}
+	if(req.query.area != null && req.query.area !== ''){
+		search.area = new RegExp(req.query.area , 'i')
+	}
+
+	const allHouse = await rental.find(search).limit(2).sort({$natural:-1})
+	res.render('index', {allHouse: allHouse,
+	                        search: search
+	                    })
 })
 
 module.exports = router
