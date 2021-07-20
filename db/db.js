aa989fc532d45cdc9fc9234305198902a92767cc
ian onyango <61430847+ian-yitzhak@users.noreply.github.com>
2021-07-20 15:13:04 +0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,8 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 mongoose.connect(
-    "mongodb+srv://todojs:<password>@cluster0.xlsfe.mongodb.net/taskapp?retryWrites=true&w=majority", 
-    { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true  }
-)
- .then(console.log('connected succesfully'))
- .catch(err=> console.log(err))
+	"mongodb+srv://ian_test:METH71@sellah@ian.qfm7u.mongodb.net/auth?retryWrites=true&w=majority",
+	{ useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true  }
+
+	)
+.then(()=> console.log('Connected successfully'))
+.catch((err)=> console.log(err))
+
